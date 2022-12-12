ab5712a0c2f1e0e973927cc62ceca12b928646ff
MinhyunAhn <mhahn@crossteam.co.kr>
2022-12-12 10:13:57 +0900

    fix

--- example/android/app/src/main/kotlin/com/amh/flutter_image_editor_plugin_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.amh.flutter_image_editor_plugin_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/flutter_image_editor_plugin.dart
+++
@@ -1,4 +0,0 @@
-library flutter_image_editor_plugin;
-
-export 'draw_line.dart';
-export 'image_editor_painter.dart';
